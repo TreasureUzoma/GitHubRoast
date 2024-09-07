476ac6e242c70fa73dc20b596a62ea36b3e82254
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-09-07 18:09:38 +0100

    Update utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -10,17 +10,8 @@ import {
     updateDoc
 } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 
-const API_KEY = process.env.API_KEY;
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const API_KEY = process.env.REACT_APP_API_KEY;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
